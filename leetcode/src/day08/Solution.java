47be3706e5c243942c6bf657eb1917366a9ee93b
张浩锐 <Zhr199707>
2021-06-01 09:25:35 +0800

    1744. 你能在你最喜欢的那天吃到你最喜欢的糖果吗？

---
+++ leetcode/src/day08/Solution.java
@@ -0,0 +1,54 @@
+package day08;
+
+/**
+ * @ClassName Solution8
+ * @Author zhanghaorui
+ * @Date 2021/6/1 9:23 上午
+ * @Description
+ *          给你一个下标从 0 开始的正整数数组 candiesCount ，其中 candiesCount[i] 表示你拥有的第 i 类糖果的数目。同时给你一个二维数组 queries ，其中 queries[i] = [favoriteTypei, favoriteDayi, dailyCapi] 。
+ *
+ *          你按照如下规则进行一场游戏：
+ *
+ *          你从第 0 天开始吃糖果。
+ *          你在吃完 所有 第 i - 1 类糖果之前，不能 吃任何一颗第 i 类糖果。
+ *          在吃完所有糖果之前，你必须每天 至少 吃 一颗 糖果。
+ *          请你构建一个布尔型数组 answer ，满足 answer.length == queries.length 。answer[i] 为 true 的条件是：在每天吃 不超过 dailyCapi 颗糖果的前提下，你可以在第 favoriteDayi 天吃到第 favoriteTypei 类糖果；否则 answer[i] 为 false 。注意，只要满足上面 3 条规则中的第二条规则，你就可以在同一天吃不同类型的糖果。
+ *
+ *          请你返回得到的数组 answer 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/can-you-eat-your-favorite-candy-on-your-favorite-day
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @Version 1.0
+ */
+public class Solution {
+
+    public boolean[] canEat(int[] candiesCount, int[][] queries) {
+        int n = candiesCount.length;
+
+        // 前缀和
+        long[] sum = new long[n];
+        sum[0] = candiesCount[0];
+        for (int i = 1; i < n; ++i) {
+            sum[i] = sum[i - 1] + candiesCount[i];
+        }
+
+        int q = queries.length;
+        boolean[] ans = new boolean[q];
+        for (int i = 0; i < q; ++i) {
+            int[] query = queries[i];
+            int favoriteType = query[0], favoriteDay = query[1], dailyCap = query[2];
+
+            long x1 = favoriteDay + 1;
+            long y1 = (long) (favoriteDay + 1) * dailyCap;
+            long x2 = favoriteType == 0 ? 1 : sum[favoriteType - 1] + 1;
+            long y2 = sum[favoriteType];
+
+            ans[i] = !(x1 > y2 || y1 < x2);
+        }
+        return ans;
+
+    }
+
+}
